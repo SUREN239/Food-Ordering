086dfbb8ca90f06253b2d9a57e74e90878359658
SUREN S <121883751+SUREN239@users.noreply.github.com>
2024-04-01 14:30:56 +0530

    Delete foodordering directory

--- foodordering/src/main/java/suren/foodordering/repository/UserRegRepo.java
+++
@@ -1,9 +0,0 @@
-package suren.foodordering.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import suren.foodordering.model.UserReg;
-
-public interface UserRegRepo extends JpaRepository<UserReg, Long>
-{
-
-}
